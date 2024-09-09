27e5dee49d321654862051c6222b826264112c8a
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2024-09-10 01:25:52 +0300

    sum

--- Assets/Scripts/Slider/PagesStream.cs
+++ Assets/Scripts/Slider/PagesStream.cs
@@ -12,14 +12,14 @@ namespace SliderPages
         public delegate void StreamsLoadedHandler();
         public static event StreamsLoadedHandler e_streamsLoaded;
 
-        private string pathToEntryWhiteMemorial = "1.EntryWhiteMemorial";
-        private string pathToEternalFlame = "2.EternalFlame";
-        private string pathToFiveBlackMemorials = "3.FiveBlackMemorials";
-        private string pathToTwoWhiteSculptures = "4.TwoWhiteSculptures";
-        private string pathToBlackMemorialBehindWhiteSculpture = "5.BlackMemorialBehindWhiteSculpture";
-        private string pathTo76mmDivisionalGun = "6.76mmDivisionalGun";
-        private string pathToMainManument = "7.MainManument";
-        private string pathToSecondManument = "8.SecondManument";
+        //private string pathToEntryWhiteMemorial = "1.EntryWhiteMemorial";
+        //private string pathToEternalFlame = "2.EternalFlame";
+        //private string pathToFiveBlackMemorials = "3.FiveBlackMemorials";
+        //private string pathToTwoWhiteSculptures = "4.TwoWhiteSculptures";
+        //private string pathToBlackMemorialBehindWhiteSculpture = "5.BlackMemorialBehindWhiteSculpture";
+        //private string pathTo76mmDivisionalGun = "6.76mmDivisionalGun";
+        //private string pathToMainManument = "7.MainManument";
+        //private string pathToSecondManument = "8.SecondManument";
 
         private string pathToVideo = "Video";
 
@@ -37,60 +37,60 @@ namespace SliderPages
 
         private void Start()
         {
-            GetFileLocation(pathToEntryWhiteMemorial, EntryWhiteMemorialSprites);
-            GetFileLocation(pathToEternalFlame, EternalFlame);
-            GetFileLocation(pathToFiveBlackMemorials, FiveBlackMemorials);
-            GetFileLocation(pathToTwoWhiteSculptures, TwoWhiteSculptures);
-            GetFileLocation(pathToBlackMemorialBehindWhiteSculpture, BlackMemorialBehindWhiteSculpture);
-            GetFileLocation(pathTo76mmDivisionalGun, DivisionalGun);
-            GetFileLocation(pathToMainManument, MainManument);
-            GetFileLocation(pathToSecondManument, SecondManument);
+            //GetFileLocation(pathToEntryWhiteMemorial, EntryWhiteMemorialSprites);
+            //GetFileLocation(pathToEternalFlame, EternalFlame);
+            //GetFileLocation(pathToFiveBlackMemorials, FiveBlackMemorials);
+            //GetFileLocation(pathToTwoWhiteSculptures, TwoWhiteSculptures);
+            //GetFileLocation(pathToBlackMemorialBehindWhiteSculpture, BlackMemorialBehindWhiteSculpture);
+            //GetFileLocation(pathTo76mmDivisionalGun, DivisionalGun);
+            //GetFileLocation(pathToMainManument, MainManument);
+            //GetFileLocation(pathToSecondManument, SecondManument);
 
             GetVideoFileLocation(pathToVideo, VideoFiles);
 
             e_streamsLoaded?.Invoke();
         }
 
-        public void GetFileLocation(string pathToTexturesForMerkersSlider, List<Sprite> listSprites)
-        {
-            // Получаем путь к директории в StreamingAssets
-            string fullPath = Path.Combine(Application.streamingAssetsPath, pathToTexturesForMerkersSlider);
-            DirectoryInfo directoryInfo = new DirectoryInfo(fullPath);
+        //public void GetFileLocation(string pathToTexturesForMerkersSlider, List<Sprite> listSprites)
+        //{
+        //    // Получаем путь к директории в StreamingAssets
+        //    string fullPath = Path.Combine(Application.streamingAssetsPath, pathToTexturesForMerkersSlider);
+        //    DirectoryInfo directoryInfo = new DirectoryInfo(fullPath);
 
-            // Проверяем, существует ли директория
-            if (!directoryInfo.Exists)
-            {
-                //Debug.LogError("Directory not found: " + fullPath);
-                return;
-            }
+        //    // Проверяем, существует ли директория
+        //    if (!directoryInfo.Exists)
+        //    {
+        //        //Debug.LogError("Directory not found: " + fullPath);
+        //        return;
+        //    }
 
-            // Получаем все файлы .png, .jpg, и .jpeg в директории
-            FileInfo[] allFiles = directoryInfo.GetFiles("*.*")
-                                               .Where(file => file.Extension.Equals(".png", StringComparison.OrdinalIgnoreCase) ||
-                                                              file.Extension.Equals(".jpg", StringComparison.OrdinalIgnoreCase) ||
-                                                              file.Extension.Equals(".jpeg", StringComparison.OrdinalIgnoreCase))
-                                               .ToArray();
+        //    // Получаем все файлы .png, .jpg, и .jpeg в директории
+        //    FileInfo[] allFiles = directoryInfo.GetFiles("*.*")
+        //                                       .Where(file => file.Extension.Equals(".png", StringComparison.OrdinalIgnoreCase) ||
+        //                                                      file.Extension.Equals(".jpg", StringComparison.OrdinalIgnoreCase) ||
+        //                                                      file.Extension.Equals(".jpeg", StringComparison.OrdinalIgnoreCase))
+        //                                       .ToArray();
 
-            // Загружаем изображения и создаем спрайты
-            foreach (FileInfo file in allFiles)
-            {
-                string filePath = file.FullName;
+        //    // Загружаем изображения и создаем спрайты
+        //    foreach (FileInfo file in allFiles)
+        //    {
+        //        string filePath = file.FullName;
 
-                // Загружаем текстуру из файла
-                byte[] fileData = File.ReadAllBytes(filePath);
-                Texture2D texture = new Texture2D(2, 2);
-                texture.LoadImage(fileData);
+        //        // Загружаем текстуру из файла
+        //        byte[] fileData = File.ReadAllBytes(filePath);
+        //        Texture2D texture = new Texture2D(2, 2);
+        //        texture.LoadImage(fileData);
 
-                // Создаем спрайт из текстуры
-                Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-                // Присваиваем спрайту имя файла (без расширения)
-                sprite.name = Path.GetFileNameWithoutExtension(file.Name);
-                // Добавляем спрайт в список
-                listSprites.Add(sprite);
-            }
+        //        // Создаем спрайт из текстуры
+        //        Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+        //        // Присваиваем спрайту имя файла (без расширения)
+        //        sprite.name = Path.GetFileNameWithoutExtension(file.Name);
+        //        // Добавляем спрайт в список
+        //        listSprites.Add(sprite);
+        //    }
 
-            //Debug.Log("Loaded " + listSprites.Count + " sprites.");
-        }
+        //    //Debug.Log("Loaded " + listSprites.Count + " sprites.");
+        //}
 
         public void GetVideoFileLocation(string pathToVideos, List<string> videoList)
         {
